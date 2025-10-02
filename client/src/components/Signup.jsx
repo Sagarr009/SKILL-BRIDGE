37b75da1e8bdd2e7727e4c394eb9e103ad165082
Sagar Gupta <156563894+Sagarr009@users.noreply.github.com>
2025-10-02 09:13:40 +0530

    Update Signup.jsx

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -63,7 +63,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/auth/signup', {
+      await axios.post('https://skill-bridge-back.onrender.com/api/auth/signup', {
         ...formData,
         skillsCanTeach: [formData.skillsCanTeach],
         skillsWantToLearn: [formData.skillsWantToLearn],
